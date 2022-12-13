2e3a02b13a4b5594a819f99a27b2835646ea6a73
April Ashton <aprilashton18@gmail.com>
2022-12-13 04:19:55 -0800

    commit tree d644a577b75dd3079394b5b08f706f5bfa06a88a

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(num){
   // Write your code here!
+  if (num <= 400) {
+    return "This one is on me!";
+  } else if (num > 400 && num <= 2000) {
+    return "That will be twenty bucks.";
+  } else if (num > 2000 && num < 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else {
+    return "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let message = city === "NYC" ? ("Ok, sounds good.") : ("No go.");
+  return message;
+
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch(tip) {
+    case "generous" :
+      return "Thank you so much.";
+      break;
+    case "not as generous" :
+      return "Thank you.";
+      break;
+    default:
+      return "Bye."
+  }
 }
